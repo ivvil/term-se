0f0894a6f2c786f35860048f0e22ff6ba3b26952
Iván Villagrasa <ivvil412@gmail.com>
2025-02-21 00:45:22 +0100

    fixup! Build stuff

---
+++ src/server.cpp
@@ -0,0 +1,11 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+}
